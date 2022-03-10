221306411c2178b8ccd641a011f3458813c0480b
Adenilson Leandro Junior <adenilsonlj@hotmail.com>
2022-03-10 08:12:34 -0300

    first commit

---
+++ src/Example.API/Program.cs
@@ -0,0 +1,38 @@
+using Example.Application.ExampleService.Service;
+using Example.Infra.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IExampleService, ExampleService>();
+builder.Services.AddDbContext<ExampleContext>(o => o.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var dataContext = scope.ServiceProvider.GetRequiredService<ExampleContext>();
+    dataContext.Database.Migrate();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
